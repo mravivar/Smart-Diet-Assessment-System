aecdbed8015d5fac03c66c919ef8c22cbbc25e8b
Shashaank Dandapani Sivakumar (Student) <sdandapa@sundevils.asu.edu>
2017-10-24 15:54:37 -0700

    Code for adding headers is COMPLETE

--- addHeaders.m
+++ addHeaders.m
@@ -1,36 +1,49 @@
-%addHeadersToFile('C:\Users\sdandapa\Desktop\eating_subset.csv')
-% function addHeadersToFile(ifilename)
-    ifilename = "eating.csv";
-    
-    f = csvread(ifilename);
-    for i=1:size(f)
-        toAdd = ["action 1 acc X"
-             "action 1 acc Y"
-             "action 1 acc Z"
-             "action 1 acc W"
-             "action 1 gy x"
-             "action 1 gy y"
-             "action 1 gy z"
-             "action 1 ori x"
-             "action 1 ori y"
-             "action 1 ori z"
-             "action 1 emg 1"
-             "action 1 emg 2"
-             "action 1 emg 3"
-             "action 1 emg 4"
-             "action 1 emg 5"
-             "action 1 emg 6"
-             "action 1 emg 7"
-             "action 1 emg 8"
-            ];
-        temp = f(i:i+17,:);
-        final = [toAdd temp];
-        
-        % Find a way to append to csv file or excel file. Just need to
-        % write final in every loop!
-        %xlswrite("final_eating.xlsx", final);  
-        %xlmwrite("final_eating.xlsx", final, 'append');   
-    
-    end
-     
-%end
+ifilename = "eating.csv";
+f = csvread(ifilename);
+i = 1
+toAdd = ["action "+i+" acc X"
+         "action "+i+" acc Y"
+         "action "+i+" acc Z"
+         "action "+i+" acc W"
+         "action "+i+" gy x"
+         "action "+i+" gy y"
+         "action "+i+" gy z"
+         "action "+i+" ori x"
+         "action "+i+" ori y"
+         "action "+i+" ori z"
+         "action "+i+" emg 1"
+         "action "+i+" emg 2"
+         "action "+i+" emg 3"
+         "action "+i+" emg 4"
+         "action "+i+" emg 5"
+         "action "+i+" emg 6"
+         "action "+i+" emg 7"
+         "action "+i+" emg 8"
+        ];
+
+for i=2:size(f,1)/18
+    toAdd = [toAdd ; "action "+i+" acc X"
+         "action "+i+" acc Y"
+         "action "+i+" acc Z"
+         "action "+i+" acc W"
+         "action "+i+" gy x"
+         "action "+i+" gy y"
+         "action "+i+" gy z"
+         "action "+i+" ori x"
+         "action "+i+" ori y"
+         "action "+i+" ori z"
+         "action "+i+" emg 1"
+         "action "+i+" emg 2"
+         "action "+i+" emg 3"
+         "action "+i+" emg 4"
+         "action "+i+" emg 5"
+         "action "+i+" emg 6"
+         "action "+i+" emg 7"
+         "action "+i+" emg 8"
+        ];
+
+end
+
+final = [toAdd f];
+cell2csv('output3.csv',cellstr(final));
+
